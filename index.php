8111eb598be2d95aa0d17fb57c8c4f46cfd3a4c6
sergioralandison <sergioralandison@outlook.com>
2020-10-28 16:29:47 +0100

    structure html, container, aside, nav and content

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <title>Sergio Ralandison</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Template CSS Files -->
+    <link rel="stylesheet" href="css/main.css" type="text/css">
+    <link rel="stylesheet" href="css/skins/pink.css" type="text/css">
+  </head>
+
+  <body>
+    <!-- Main Container -->
+    <div class="main-container">
+      <!-- Aside -->
+      <div class="aside">
+        <!-- Logo -->
+        <div class="logo">
+          <a href="#">Sergio</a>
+        </div>
+        <!-- Nav Toggler Btn -->
+        <div class="nav-toggler">
+          <span></span>
+        </div>
+        <!-- Nav -->
+        <ul class="nav">
+          <li><a href="#" class="active"><i class="fa fa-home"></i>Home</a></li>
+          <li><a href="#"><i class="fa fa-user"></i>About</a></li>
+          <li><a href="#"><i class="fa fa-briefcase"></i>Portfolio</a></li>
+          <li><a href="#"><i class="fa fa-comments"></i>Contact</a></li>
+        </ul>
+      </div>
+      <!-- Aside End-->
+
+      <!-- Main Content -->
+      <div class="main-content">
+        <!-- Home Section  -->
+        <section class="home section">
+          <div class="container">
+            <div class="intro">
+              <img src="images/profile.png" alt="profile" class="shadow-dark">
+              <h1>Sergio Ralandison</h1>
+              <p>I'm a Web Developer & Designer</p>
+              <div class="social-links">
+                <a href="#"><i class="fa fa-twitter"></i></a>
+                <a href="#"><i class="fa fa-linkedin"></i></a>
+                <a href="#"><i class="fa fa-github"></i></a>
+              </div>
+            </div>
+          </div>
+        </section>
+        <!-- Home Section End -->
+      </div>
+      <!-- Main Content End -->
+    </div>
+    <!-- Main Container -->
+  </body>
+</html>
